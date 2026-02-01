2ed5355af861fa0945da618d0d90f6acf0da8936
kernjason4460-cyber <kernjason4460@gmail.com>
2026-02-01 12:16:47 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// js/config.js
+export const FBZ_API_URL =
+  "https://script.google.com/macros/s/AKfycbw2PCr8A2ODkE-nPqKAGqKKn7ZNLTxcTDk1AQygGozHro4zYQuZCbxZ_BnTPuVS-mNF/exec";
+
+// Optional for later (you can leave it blank for now)
+export const FBZ_API_KEY = "";
